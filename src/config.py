8427f695d79f35edc1147b7e7c1e339d1b957f5c
eduard <eduard.babayan@cognaize.com>
2024-07-30 13:20:05 +0400

    ADDED new path attributes to CFG class

---
+++ src/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+class CFG:
+    root = Path(__file__).parent.parent.absolute()
+    data_dir = root.joinpath('data')
+    preprocessing_dir = root.joinpath('preprocessed')
+
