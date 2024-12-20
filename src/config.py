05d225e07f253ea351d454c058b19189fa291393
eduard <eduard.babayan@cognaize.com>
2024-12-20 16:08:55 +0400

    ADD: new configurations

--- src/config.py
+++ src/config.py
@@ -6,4 +6,16 @@ class CFG:
     data_dir = root.joinpath('data')
     preprocessing_dir = root.joinpath('preprocessed')
 
+    TEST_FILE = "train.csv"
+
     trained_models_dir = root.joinpath('trained_models')
+
+    MODEL_NAME = 'loanability_detector.pkl'
+
+    TARGET = 'Loan_Status'
+
+    FEATURE_TO_ADD = ['residential_assets_value', 'commercial_assets_value', 'luxury_assets_value', 'bank_asset_value']
+
+    DROP_FEATURES = ['residential_assets_value', 'commercial_assets_value', 'luxury_assets_value', 'bank_asset_value']
+
+    LOG_FEATURES = ['income_annum', 'loan_amount', 'total_assets_value']
